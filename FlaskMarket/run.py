8795c2dd93154c1381a1bdf6066b90d49ef1b921
TechWithJin <130134152+TechWithJin@users.noreply.github.com>
2023-09-22 13:38:11 +0900

    Add files via upload

--- FlaskMarket/run.py
+++ FlaskMarket/run.py
@@ -1,9 +1,5 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from market import app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-db = SQLAlchemy(app)
 
 
 
